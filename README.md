3d337aadf9e35be4ebc986f75c329e1b13ada3e6
Amiran Gorgazjan <amiran@deepinsight.io>
2022-11-06 21:57:32 +0100

    Probot as express middleware for future statistics and debugging.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Git Code Review Bot
+
+# Heroku secrets
+
+```bash
+heroku config:set APP_ID=aaa
+heroku config:set WEBHOOK_SECRET=bbb
+heroku config:set PRIVATE_KEY="$(cat ~/Downloads/*.private-key.pem)"
+```
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,25 @@
 // when a pull request is opened.
 // https://probot.github.io/docs/hello-world/
 
-import { run } from "probot";
-import app from "./app";
+import { createNodeMiddleware, Probot } from "probot";
+import probotApp from "./app";
+import express from "express";
 
-run(app)
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(createNodeMiddleware(probotApp, {
+    probot: new Probot({
+        appId: process.env.APP_ID,
+        secret: process.env.WEBHOOK_SECRET,
+        privateKey: process.env.PRIVATE_KEY,
+    })
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
